297655b6e55adba2007cc3a36c5b1ed5a8956e69
Rein-xmmm <72437331+Rein-xmmm@users.noreply.github.com>
2020-10-18 10:06:46 +0900

    Initial commit

---
+++ FunctionInsp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InspectorLib;
+using System.Threading;
+
+namespace FunctionInsp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 1; x <= 100; x++)
+            {
+                Console.WriteLine($"Пожождите идет загрузка данных");
+                Console.Write($"{x}%");
+                Thread.Sleep(3);
+                Console.Clear();
+            }
+            FunctionInsp funk = new FunctionInsp();
+            FunctionInsp.GetInspector();
+            FunctionInsp.GetСarInspection();
+            FunctionInsp.SetInspector();
+            Console.WriteLine($"{funk.GenerateNumber("028", "s".ToUpper(), "75")}");
+            FunctionInsp.GetWorker();
+            Console.WriteLine($"{funk.AddWdorker()}");
+
+            Console.ReadLine();  
+        }
+    }
+}
